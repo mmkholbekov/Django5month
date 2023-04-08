f0f769a937e802f172b73b94c9a31a37d1dfc7a9
mmkholbekov <120006110+mmkholbekov@users.noreply.github.com>
2023-04-08 15:43:22 +0400

    Hw3 Django 5mo

--- product/views.py
+++ product/views.py
@@ -6,62 +6,116 @@ from product.serializers import ProductSerializer, CategorySerializer, ReviewSer
     ProductsReviewsSerializer
 
 
-@api_view(['GET'])
-def category_list(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(data=serializer.data)
+@api_view(['GET', 'POST'])
+def category_list_api_view(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        name = request.data.get('name')
+        category = Category.objects.create(name=name)
+        return Response(data=CategorySerializer(category).data)
 
 
-@api_view(['GET'])
-def category_detail(request, id):
+@api_view(['GET','PUT', 'DELETE'])
+def category_detail_api_view(request, id):
     try:
         category = Category.objects.get(id=id)
     except Category.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND,
                         data="Error! Page not found")
-    serializer = CategorySerializer(category)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(data=serializer.data)
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        category.name = request.data.get('name')
+        category.save()
+        return Response(data=CategorySerializer(category).data)
 
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(data=serializer.data)
+@api_view(['GET', 'POST'])
+def product_list_api_view(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        title = request.data.get('title')
+        description = request.data.get('description')
+        price = request.data.get('price')
+        category = request.data.get('category')
+        category_id = request.data.get('category_id')
+        product = Product.objects.create(title=title, description=description,
+                                         price=price, category=category, category_id=category_id)
+        return Response(data=ProductSerializer(product).data)
 
 
-@api_view(['GET'])
-def product_detail(request, id):
+
+@api_view(['GET','PUT', 'DELETE'])
+def product_detail_api_view(request, id):
     try:
         product = Product.objects.get(id=id)
     except Product.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND,
                         data="Error! Page not found")
-    serializer = ProductSerializer(product)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(data=serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        product.title = request.data.get('title')
+        product.description = request.data.get('description')
+        product.price = request.data.get('price')
+        product.category = request.data.get('category')
+        product.category_id = request.data.get('category_id')
+        product.save()
+        return Response(data=ProductSerializer(product).data)
 
 
-@api_view(['GET'])
-def review_list(request):
-    review = Review.objects.all()
-    serializer = ReviewSerializer(review, many=True)
-    return Response(data=serializer.data)
+@api_view(['GET', 'POST'])
+def review_list_api_view(request):
+    if request.method == 'GET':
+        review = Review.objects.all()
+        serializer = ReviewSerializer(review, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        text = request.data.get('text')
+        product = request.data.get('product')
+        stars = request.data.get('stars')
+        product_id = request.data.get('product_id')
+        review = Review.objects.create(text=text, product=product, stars=stars, product_id=product_id)
+        return Response(data=ReviewSerializer(review).data)
 
 
-@api_view(['GET'])
-def review_detail(request, id):
+@api_view(['GET','PUT', 'DELETE'])
+def review_detail_api_view(request, id):
     try:
         review = Review.objects.get(id=id)
     except Review.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND,
                         data="Error! Page not found")
-    serializer = ReviewSerializer(review)
-    return Response(data=serializer.data)
-
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(data=serializer.data)
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        review.text = request.data.get('text')
+        review.product = request.data.get('product')
+        review.stars = request.data.get('stars')
+        review.product_id = request.data.get('product_id')
+        review.save()
+        return Response(data=ReviewSerializer(review).data)
 
 @api_view(['GET'])
-def products_reviews(request):
+def products_reviews_api_view(request):
     products = Product.objects.all().prefetch_related('reviews')
     serializer = ProductsReviewsSerializer(products, many=True)
     return Response(data=serializer.data)
--- shop_api/urls.py
+++ shop_api/urls.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
 from django.urls import path
 from product import views
+# from .views import category_list,
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/categories/', views.category_list),
-    path('api/v1/categories/<int:id>/', views.category_detail),
-    path('api/v1/products/', views.product_list),
-    path('api/v1/products/<int:id>/', views.product_detail),
-    path('api/v1/reviews/', views.review_list),
-    path('api/v1/reviews/<int:id>/', views.review_detail),
-    path('api/v1/products/reviews/', views.products_reviews),
+    path('api/v1/categories/', views.category_list_api_view),
+    path('api/v1/categories/<int:id>/', views.category_detail_api_view),
+    path('api/v1/products/', views.product_list_api_view),
+    path('api/v1/products/<int:id>/', views.product_detail_api_view),
+    path('api/v1/reviews/', views.review_list_api_view),
+    path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
+    path('api/v1/products/reviews/', views.products_reviews_api_view),
 ]
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/categories/', views.category_list, name='categories' ),
+#     path('api/v1/categories/<int:id>/', views.category_detail, name='category_detail'),
+#     path('api/v1/products/', views.product_list, name='product_list'),
+#     path('api/v1/products/<int:id>/', views.product_detail, name='product_detail'),
+#     path('api/v1/reviews/', views.review_list, name='review_list'),
+#     path('api/v1/reviews/<int:id>/', views.review_detail, name='review_detail'),
+#     path('api/v1/products/reviews/', views.products_reviews),
+# ]
