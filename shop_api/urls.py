d2f1f74119a994852e402f519cdf90aed88dbb3f
mmkholbekov <120006110+mmkholbekov@users.noreply.github.com>
2023-04-08 15:46:53 +0400

    Hw3 Django 5mo

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from product import views
-# from .views import category_list,
 
 
 urlpatterns = [
@@ -14,15 +13,3 @@ urlpatterns = [
     path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
     path('api/v1/products/reviews/', views.products_reviews_api_view),
 ]
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/v1/categories/', views.category_list, name='categories' ),
-#     path('api/v1/categories/<int:id>/', views.category_detail, name='category_detail'),
-#     path('api/v1/products/', views.product_list, name='product_list'),
-#     path('api/v1/products/<int:id>/', views.product_detail, name='product_detail'),
-#     path('api/v1/reviews/', views.review_list, name='review_list'),
-#     path('api/v1/reviews/<int:id>/', views.review_detail, name='review_detail'),
-#     path('api/v1/products/reviews/', views.products_reviews),
-# ]
